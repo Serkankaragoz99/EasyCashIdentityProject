72ed611029f16db72632b715eca2703e6e664aa5
Serkankaragoz99 <103015554+Serkankaragoz99@users.noreply.github.com>
2023-05-23 19:46:04 +0300

    Ders 18

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController .cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController .cs
@@ -63,7 +63,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 
 					SmtpClient client = new SmtpClient();
 					client.Connect("smtp.gmail.com", 587, false);
-					client.Authenticate("gitarisserkan@gmail.com", "okgclatwwjwdwmqy");
+					client.Authenticate("gitarisserkan@gmail.com", "bqnşişlkğüpo");
 					client.Send(mimeMessage);
 					client.Disconnect(true);
 
